e79a7e1620b7e4eebc6bcb2597acb2c2cb3507c7
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2016-07-06 17:06:14 -0500

    foo made

---
+++ src/dog.js
@@ -0,0 +1,6 @@
+export default class Dog (){
+
+  says: 'life is ruff';
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 'use strict';
+import Foo from 'foo';
+import Dog from 'dog';
+// var bar = new Foo();
+ // console.log(dog);
