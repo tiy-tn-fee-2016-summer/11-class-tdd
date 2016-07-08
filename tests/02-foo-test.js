0812be9e711ab2742316735e18e17b44c9903c31
Hannah Swift <h.graceswift@gmail.com>
2016-07-08 09:04:45 -0500

    Create instance of Foo

--- tests/02-foo-test.js
+++ tests/02-foo-test.js
@@ -1,15 +1,14 @@
-export default class Foo {
-  (function () {
-    'use strict';
+(function () {
+  'use strict';
 
-    /* global require */
-    const Foo = require('foo');
+  /* global require */
+  const Foo = require('foo');
+  const bar = new Foo;
 
-    test('There is a Foo class and an instance called bar', (assert) => {
-      // Create an instance of Foo and set it to the variable "bar"
+  test('There is a Foo class and an instance called bar', (assert) => {
+    // Create an instance of Foo and set it to the variable "bar"
 
-      assert.ok(bar instanceof Foo,
-        'Create a simple class called \'Foo\'and create a new instance from it called \'bar\'');
-    });
-  }());
-}
+    assert.ok(bar instanceof Foo,
+      'Create a simple class called \'Foo\'and create a new instance from it called \'bar\'');
+  });
+}());
