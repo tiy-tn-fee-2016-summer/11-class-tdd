af6e9a3ca312af142e307fccdf53402ca15c1b9b
Hannah Swift <h.graceswift@gmail.com>
2016-07-07 20:11:50 -0500

    Change title

--- tests/02-foo-test.js
+++ tests/02-foo-test.js
@@ -1,13 +1,15 @@
-(function () {
-  'use strict';
+export default class Foo {
+  (function () {
+    'use strict';
 
-  /* global require */
-  const Foo = require('foo');
+    /* global require */
+    const Foo = require('foo');
 
-  test('There is a Foo class and an instance called bar', (assert) => {
-    // Create an instance of Foo and set it to the variable "bar"
+    test('There is a Foo class and an instance called bar', (assert) => {
+      // Create an instance of Foo and set it to the variable "bar"
 
-    assert.ok(bar instanceof Foo,
-      'Create a simple class called \'Foo\'and create a new instance from it called \'bar\'');
-  });
-}());
+      assert.ok(bar instanceof Foo,
+        'Create a simple class called \'Foo\'and create a new instance from it called \'bar\'');
+    });
+  }());
+}
